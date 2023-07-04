777ae2901b6688a62ab5ad02a92300e29c7c8e9e
adityaputra42 <aditiyaputra42@gmail.com>
2023-07-04 23:20:44 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/adityaputra42/app-hello
+
+go 1.18
+
+require github.com/adityaputra42/go-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gohello "github.com/adityaputra42/go-hello/v2"
+)
+
+func main() {
+	fmt.Println(gohello.SayHello("Amanda"))
+}
